da898a718277c15aae7a1107a679906fe3a9a6ea
Wouter Van Rooy <vanrooywouter@gmail.com>
2012-05-28 12:21:56 +0200

    Make readlink warning dissapear
    It's standard in gnu99 but not in c99.

--- config.mk
+++ config.mk
@@ -36,7 +36,7 @@ INCS = -I. -I/usr/include ${GTKINC}
 LIBS = -L/usr/lib -lc ${GTKLIB}
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-CFLAGS = -mtune=native -std=c99 -O3 ${INCS} ${CPPFLAGS}
+CFLAGS = -mtune=native -std=gnu99 -O3 ${INCS} ${CPPFLAGS}
 LDFLAGS = -s ${LIBS}
 
 # Solaris
